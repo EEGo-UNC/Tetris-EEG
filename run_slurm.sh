f54b91be79c4fa6c8ec180bb5a43896260188607
VitorInserra <inserra.vitor@gmail.com>
2026-01-20 16:03:29 -0500

    Edit run slurm to relative path

--- run_slurm.sh
+++ run_slurm.sh
@@ -9,9 +9,12 @@
 #SBATCH --gres=gpu:1
 
 set -euo pipefail
+
+cd /users/i/n/inserra/Tetris-EEG
+
 mkdir -p logs
 
-source /venv/bin/activate
+source .venv/bin/activate
 
 export TF_FORCE_GPU_ALLOW_GROWTH=true
 
